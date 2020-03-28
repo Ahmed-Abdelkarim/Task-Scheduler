df5449aa160f7e046158265b6e2752397cc6fa3b
Ahmed Abdelkarim <ahmedabdelhafez20111@gmail.com>
2020-03-28 07:21:09 -0700

    remaining time calculated

--- OS_Starter_Code/process.c
+++ OS_Starter_Code/process.c
@@ -2,19 +2,20 @@
 
 /* Modify this file as needed*/
 int remainingtime;
-
+int runtime;
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
+    remainingtime = runtime - (clock()/CLOCKS_PER_SECOND);
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        remainingtime = runtime - (clock()/CLOCKS_PER_SECOND);
     }
-    
+
     destroyClk(false);
-    
+
     return 0;
 }
